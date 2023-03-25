3f7a6acaa506d795955c4f130908bd54b6c6e71f
marcelolynx <marcelolynx@gmail.com>
2023-03-25 10:01:15 -0400

    insert com postgres

--- go-database/main.go
+++ go-database/main.go
@@ -1,7 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "mco02jgp"
+	dbname   = "goexpert"
+)
+
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func NewProduct(name string, price float64) *Product {
+	return &Product{
+		ID:    uuid.New().String(),
+		Name:  name,
+		Price: price,
+	}
+}
 
 func main() {
-	fmt.Println("Database com GO")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
+	product := NewProduct("Notebook", 1899.90)
+	err = insertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("INSERT INTO PRODUCTS (name, price) VALUES ($1, $2)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.Name, product.Price)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Product successfully inserted!")
+	return nil
 }
